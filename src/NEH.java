56f83d6f023f16736503881888a3006556a2c325
Smeet Patel <smeetp18@gmail.com>
2020-12-04 23:41:45 -0500

    NEH class to generate initial feasible solution. Incomplete.

---
+++ src/NEH.java
@@ -0,0 +1,133 @@
+
+
+import java.util.*;
+
+public class NEH {
+    /**
+     * Performs NEH.
+     * NOTE: NOT WORKING YET. PUTTING IT UP FOR EVERYONE's REFERENCE.
+     *
+     */
+    private List<Data> processingTimes;
+    private List<Data> schedule;
+
+    NEH(List<Data> processingTimes){
+        this.processingTimes = processingTimes;
+        this.schedule = new ArrayList<>();
+    }
+
+    void getFeasibleSolution(){
+        Collections.sort(this.processingTimes, new DataComparator());
+        Data p1 = this.processingTimes.get(0);
+        Data p2 = this.processingTimes.get(1);
+        assignRelativeOrder(p1, p2);
+
+        // go through each data point and add to schedule
+        for(int i=2; i<this.processingTimes.size(); i++){
+            assignPosition(this.processingTimes.get(i));
+        }
+
+        System.out.println("Schedule:");
+        for(Data d: this.schedule){
+            System.out.println(d.getJobID());
+        }
+
+    }
+
+    void assignRelativeOrder(Data p1, Data p2){
+        /**
+         * Find correct position relative to Data point 1 and 2 to minimize makespan.
+         */
+        // if sequence p1-p2
+        int p1Sum = 0;
+        int p2Sum = 0;
+        for(int i=0; i<p1.getProcessingTimes().size(); i++){
+            p1Sum += p1.getProcessingTimes().get(i);
+            p2Sum = Math.max(p1Sum, p2Sum) + p2.getProcessingTimes().get(i);
+            System.out.println("p1sum: " + p1Sum + " | p2sum: " + p2Sum);
+        }
+        int makeSpan1 = p2Sum;
+        System.out.println("Makespan for sequence p1-p2 is: " + p2Sum);
+
+        // if sequence p2-p1
+        p1Sum = 0;
+        p2Sum = 0;
+        for(int i=0; i<p1.getProcessingTimes().size(); i++){
+            p1Sum += p2.getProcessingTimes().get(i);
+            p2Sum = Math.max(p1Sum, p2Sum) + p1.getProcessingTimes().get(i);
+            System.out.println("p1sum: " + p1Sum + " | p2sum: " + p2Sum);
+        }
+        System.out.println("Makespan for sequence p1-p2 is: " + p2Sum);
+
+        if(makeSpan1<=p2Sum){
+            this.schedule.add(p1);
+            this.schedule.add(p2);
+        } else {
+            this.schedule.add(p2);
+            this.schedule.add(p1);
+        }
+    }
+
+    void assignPosition(Data p){
+        /**
+         * Assign the data point at right point in the existing schedule without
+         * changing the order of exisiting and already allocated jobs.
+         */
+        int positionIndex = 0;
+        int correctIndex = 0;
+        int minMakeSpan = Integer.MAX_VALUE;
+        int currentMakeSpan = 0;
+
+        while(positionIndex<this.schedule.size()+1){
+            int[] makeSpans = new int[this.schedule.size()];
+            for(int i=0; i<p.getProcessingTimes().size();i++){
+                boolean checkForPI = true;
+                for(int j=0; j<this.schedule.size(); j++){
+                    //get processing time
+                    int processingTime;
+                    if(j==positionIndex && checkForPI){
+                        processingTime = p.getProcessingTimes().get(i);
+                    } else {
+                        processingTime = this.schedule.get(j).getProcessingTimes().get(i);
+                    }
+
+                    //add for first element
+                    if(j==0 && (j != positionIndex || checkForPI)){
+                        makeSpans[j] += processingTime;
+                    } else {
+                        int max = 0;
+                        for(int k=0; k<=j;k++){
+                            if(makeSpans[k]>max)
+                                max = makeSpans[k];
+                        }
+                        if(j+1>=makeSpans.length && (j != positionIndex || checkForPI)){
+                            currentMakeSpan = max + processingTime;
+                        } else{
+                            if(!(j != positionIndex || checkForPI))
+                                makeSpans[j+1] = max + processingTime;
+                            else
+                                makeSpans[j] = max + processingTime;
+                        }
+                    }
+
+                    if(j==positionIndex && checkForPI){
+                        j--;
+                        checkForPI = false;
+                    }
+                }
+                System.out.println("Result");
+                for(int f=0;f<makeSpans.length;f++){
+                    System.out.println(makeSpans[f]);
+                }
+                System.out.println(currentMakeSpan + "\n");
+            }
+            if(currentMakeSpan<minMakeSpan){
+                minMakeSpan = currentMakeSpan;
+                correctIndex = positionIndex;
+            }
+            positionIndex++;
+        }
+
+        this.schedule.add(correctIndex, p);
+    }
+}
