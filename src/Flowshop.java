f3263639af32cd19ed8b8ea4c40939d9e02e5c30
Letícia Pasdiora <leticiapasdiora@gmail.com>
2020-12-12 09:11:33 -0500

    added the tests

--- src/Flowshop.java
+++ src/Flowshop.java
@@ -1,50 +1,65 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Flowshop {
 	  public static void main (String[] args)
 	  {
-		  String path = "input/2/Ta001_2.txt";
-		  ArrayList<Integer> jobs = readFile(path);	  
-		
-		  if (jobs.size() != 0)
+		  for (int i = 0; i <= 120; i++)
 		  {
-			  // Call the methods to process the jobs
-			  
-			  // this is a test to see if it is working, we can erase that
-			  for (int i = 0; i < jobs.size(); i++)
-				  System.out.println(jobs.get(i));
+			  String path = "input/2/Ta"+ String.format("%03d", i) +"_2.txt";
+			  ArrayList<Job> jobs = readFile(path);	  
+			
+			  if (jobs.size() != 0)
+			  {
+				  NEH neh = new NEH(jobs);
+			      neh.getInitialSolution();
+
+			      // Perform iterated local search
+			      IteratedLocalSearch iteratedLocalSearch = new IteratedLocalSearch(neh.getSchedule(), neh.getMakeSpan());
+			      List<Job> solution = iteratedLocalSearch.performILS();
+				  System.out.println("Executing test " + String.format("%03d", i) + " of 120:");
+				  System.out.print("   Flow-shop Schedule: ");
+				  for (int j = 0; j < solution.size(); j++)
+					  System.out.print(solution.get(j).getJobID() + " ");
+				  System.out.println();
+				  System.out.println("   Flow-shop Makespan: " + neh.calculateMakespan(solution));
+				  System.out.println();
+			  }  
 		  }
 	  }
 	  
 	  /**
 	   * Method to parse the input file given by the path parameter
 	   * @param path - the path of the file to be opened
-	   * @return the list with the cost for each job j
+	   * @return the list of jobs, each job has a list of processing time
 	   */
-	  public static ArrayList<Integer> readFile(String path)
+	  public static ArrayList<Job> readFile(String path)
 	  {
-		  ArrayList<Integer> jobsDuration = new ArrayList<Integer>();
+		  ArrayList<Job> jobs = new ArrayList<Job>();
 		  try {
 				BufferedReader reader = new BufferedReader(new FileReader(path));
 				String line;
 				String[] jobParameters;
+				Integer id = 0;
 				while ((line = reader.readLine()) != null)
 				{
 					jobParameters = line.split("	");
-					int duration = 0;
+					ArrayList<Integer> processingTimes = new ArrayList<Integer>() ;
 					if (jobParameters.length > 2)
 					{
 						for (int i = 2; i < jobParameters.length; i+=2)
-							duration += Integer.parseInt(jobParameters[i]);
-						jobsDuration.add(duration);
+							processingTimes.add( Integer.parseInt(jobParameters[i]));
+						Job j = new Job(id , processingTimes);
+						id ++;
+						jobs.add(j);
 					}					
 				}
 				reader.close();
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		  return jobsDuration;
+		  return jobs;
 	  }
 }
