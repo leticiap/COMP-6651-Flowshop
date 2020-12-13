4c5c18f9d4dd50f17058e371ccfa91d6f24f938b
somaye <ghahary.s@gmail.com>
2020-12-13 11:44:09 +0330

    Update Flowshop.java
    refactoring

--- src/Flowshop.java
+++ src/Flowshop.java
@@ -11,11 +11,11 @@ public class Flowshop {
 	  {
 		  int fileNum = 120;
 		  System.out.println("************************ Start of program ************************\n\n");
-		  System.out.println("Running algorithm on jobs read from " + fileNum + " files\n\n");
+		  System.out.println("Running algorithm on jobs read from " + fileNum/3 + " files\n\n");
 		  long totalStart = System.currentTimeMillis();
 		  BufferedWriter bw = new BufferedWriter(new FileWriter("output.txt"));
 
-		  for (int i = 0; i <= fileNum; i++)
+		  for (int i = 0; i <= fileNum; i=i+3)
 		  {
 			  String path = "input/2/Ta"+ String.format("%03d", i) +"_2.txt";
 			  ArrayList<Job> jobs = readFile(path);	  
@@ -32,7 +32,7 @@ public class Flowshop {
 			      List<Job> solution = iteratedLocalSearch.performILS();
 				  long duration = System.currentTimeMillis()-s;
 
-				  String out = "\n\nExecuting test " + String.format("%03d", i) + " of "+fileNum+":\n"+
+				  String out = "\n\nExecuting test " + String.format("%03d", i) + ":\n"+
 						  "   Flow-shop Schedule: ";
 //				  System.out.println("Executing test " + String.format("%03d", i) + " of 120:");
 //				  System.out.print("   Flow-shop Schedule: ");
